6961e3faa7233a399a9a4fa92f1246b7d426f7ac
Kuntesh Patel <khpatel@gmail.com>
2020-07-02 15:00:01 -0400

    fixed cors issue

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = 3000
 const graphqlHTTP = require('express-graphql'); 
 const schema = require('./schema'); 
 const mongoose = require('mongoose');
+var cors = require('cors');
 
-
-
+app.use(cors())
 mongoose.connect('mongodb+srv://test:test@123@cluster0.u9ib3.mongodb.net/test',{ useNewUrlParser: true });
 mongoose.connection.once('open',() => {
     console.log('Connected');
